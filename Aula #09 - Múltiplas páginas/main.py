2475fe0c37687febd2191e569504cc38f3d47407
Guilherme Trevisan Linhares <91161693+GTL98@users.noreply.github.com>
2023-12-30 18:03:15 -0300

    Create main.py

---
+++ Aula #09 - Múltiplas páginas/main.py
@@ -0,0 +1,8 @@
+# --- Importar a biblioteca --- #
+import streamlit as st
+
+# --- Título da aula --- #
+st.title('Aula #09 - Múltiplas páginas')
+
+# --- Criar o menu lateral --- #
+st.sidebar.title('Menu')
